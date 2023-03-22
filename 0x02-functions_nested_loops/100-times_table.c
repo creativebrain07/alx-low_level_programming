f1bfeeb2cca010034d19cad4b8940cc9697bed24
creativebrain07 <berrahoumohammed9@gmail.com>
2023-03-21 17:22:48 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,9 +7,7 @@
 */
 
 void print_times_table(int n)
-
 {
-
 int i, j, k;
 if (n >= 0 && n <= 15)
 {
@@ -22,9 +20,7 @@ if (j == 0)
 {
 _putchar(k + '0');
 } 
-
 else if (k < 10 && j != 0)
-
 {
 _putchar(',');
 _putchar(' ');
@@ -32,9 +28,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 }
-
 else if (k >= 10 && k < 100)
-
 {
 _putchar(',');
 _putchar(' ');
@@ -42,9 +36,7 @@ _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 } 
-
 else if (k >= 100)
-
 {
 _putchar(',');
 _putchar(' ');
